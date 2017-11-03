0d3cc2cef7e3aa208f7de88b123f9fdece8a4527
GaryZhangYue <yuezhang@jhu.edu>
2017-11-02 23:50:33 -0400

    modified smash.py

--- tasks/smash.py
+++ tasks/smash.py
@@ -4,19 +4,23 @@
 Created on Thu Nov  2 23:02:44 2017
 
 """
+import re
+from simhash import Simhash
+
+
 '''
-#reading two genome file
+#reading two genome files
 geno_file_1, geno_file_2 = open(sys.argv[1]), open(sys.argv[2])
 file_1 = geno_file_1.readlines()
 file_2 = geno_file_2.readlines()
 '''
 
-#reading two genome file from hardcoding handle
+#reading two genome files from hardcoding handle
 geno_file_1, geno_file_2 = open('e_coli.fna'), open('s_pom.fna')
 file_1, file_2 = geno_file_1.readlines(), geno_file_2.readlines()
 
 #take the name line off and keep the sequences which is currently separated by'\n'
-#and then all sequences together as the input genome
+#and then put all sequences together as the input genome
 del file_1[0], file_2[0]
 geno_1, geno_2 = '', ''
 geno_pieces_1, geno_pieces_2 = [], []
@@ -26,3 +30,5 @@ for i in file_1:
 for i in file_2:
     geno_2 = geno_2 + i.strip()
 
+#geno_1 and geno_2 are two imported genome sequence strings
+    

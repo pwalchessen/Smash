4914e35d3c8dd26319f3e46b64853378deed08cc
GaryZhangYue <yuezhang@jhu.edu>
2017-11-05 14:20:48 -0500

    3 genome files read in, 3 short test file read in for testing, simhash called (vitual environment created for pip install Simhash and SimhashIndex

--- tasks/smash.py
+++ tasks/smash.py
@@ -9,10 +9,8 @@ Created on Thu Nov  2 23:02:44 2017
 import re, sys
 from simhash import Simhash, SimhashIndex
 
-
-'''
 #reading two genome files
-geno_file_1, geno_file_2 = open(sys.argv[1]), open(sys.argv[2]), open(sys.argv[3])
+geno_file_1, geno_file_2, geno_file_3 = open(sys.argv[1]), open(sys.argv[2]), open(sys.argv[3])
 file_1 = geno_file_1.readlines()
 file_2 = geno_file_2.readlines()
 file_3 = geno_file_3.readlines()
@@ -22,6 +20,10 @@ file_3 = geno_file_3.readlines()
 geno_file_1, geno_file_2, geno_file_3 = open('e_coli.fna'), open('s_pom.fna'), open('s_cer.fna')
 file_1, file_2, file_3 = geno_file_1.readlines(), geno_file_2.readlines(), geno_file_3.readlines()
 
+#reading the test files (short genome sequences). test_3.txt is nearly identical to test_2.txt
+geno_file_1, geno_file_2, geno_file_3 = open('test_1.txt'), open('test_2.txt'), open('test_3.txt')
+file_1, file_2, file_3 = geno_file_1.readlines(), geno_file_2.readlines(), geno_file_3.readlines()
+'''
 #take the name line off and keep the sequences which is currently separated by'\n'
 #and then put all sequences together as the input genome
 del file_1[0], file_2[0], file_3[0]
@@ -52,12 +54,17 @@ geno_3_kmers = get_features(geno_3)
 geno_1_simh_val = Simhash(geno_1_kmers).value
 geno_2_simh_val = Simhash(geno_2_kmers).value
 geno_3_simh_val = Simhash(geno_3_kmers).value
-#get dostance of two simhash
+
+'''
+#get distance of two simhash
 
 dis_geno_1_to_2 = Simhash(geno_1).distance(Simhash(geno_2))
 dis_geno_1_to_3 = Simhash(geno_1).distance(Simhash(geno_3))
 dis_geno_2_to_3 = Simhash(geno_2).distance(Simhash(geno_3))
+'''
+#simhash 3 genomes
+geno_1_simh = Simhash(geno_1_kmers)
+geno_2_simh = Simhash(geno_2_kmers)
+geno_3_simh = Simhash(geno_3_kmers)
 
 print geno_1_simh_val, ' ', geno_2_simh_val, ' ', geno_3_simh_val
-
-print dis_geno_1_to_2, ' ', dis_geno_1_to_3, ' ',dis_geno_2_to_3
